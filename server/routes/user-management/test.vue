c82b39fc69a854013fdc616c5e02f3249884df22
DESKTOP-3SB9BI9\Kuroiwa Yusaku <yousaku77@gmail.com>
2025-07-23 17:11:35 +0900

    add: members_netlify

---
+++ server/routes/user-management/test.vue
@@ -0,0 +1,12 @@
+<template lang="">
+	<div></div>
+</template>
+<script>
+if() (import.meta.hot) {
+  import.meta.hot.accept((newModule) => {
+    // Handle the hot module replacement logic here
+    console.log('Hot module replacement triggered for test.vue');
+  });
+}
+</script>
+<style lang="scss"></style>
